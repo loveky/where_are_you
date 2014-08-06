1601575158781884cfb663c5cbd5502b5ee55e25
loveky <ylzcylx@gmail.com>
2014-08-06 18:16:19 +0800

    add position translation and marker

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@
     var position = snapshot.val();
     if(position !== null) {    
       var point = new BMap.Point(position.longitude, position.latitude);
+      var marker = new BMap.Marker(point);
+      map.addOverlay(marker);
       map.centerAndZoom(point, 16);
     }
   });
@@ -28,7 +30,8 @@
     }
 
     var success = function(position) {
-      updateLocation(position.coords);
+      var point = new BMap.Point(position.coords.longitude, position.coords.latitude);
+      BMap.Convertor.translate(point, 0, translateCallback);
     };
 
     var error = function() {
@@ -36,5 +39,9 @@
     };
 
     navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  function translateCallback(position) {
+    updateLocation({longitude: position.lng, latitude: position.lat});
   }
 })(window.jQuery);

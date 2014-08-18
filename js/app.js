1ca5c9b077ba2db0248d873359589e63ace873dd
loveky <ylzcylx@gmail.com>
2014-08-18 11:00:31 +0800

    add debug window

--- js/app.js
+++ js/app.js
@@ -1,20 +1,43 @@
 (function($) {
-  var myFirebaseRef = new Firebase("https://brilliant-fire-2393.firebaseio.com/test1");
-  var dataRef = myFirebaseRef.child("position");
+  var myFirebaseRef = new Firebase("https://brilliant-fire-2393.firebaseio.com/geofile");
+  var geoFire = new GeoFire(myFirebaseRef);
+
   var map = new BMap.Map("map");
+  map.addControl(new BMap.ScaleControl());
+  map.addControl(new BMap.NavigationControl());
+
+  map.setCenter("中国");
+
+  var myName = prompt("Please enter your name");
+  var myPosition;
+  var geoQuery;
 
   dataRef.on("value", function(snapshot) {
     var position = snapshot.val();
     if(position !== null) {    
       var point = new BMap.Point(position.longitude, position.latitude);
       var marker = new BMap.Marker(point);
+
+      var markers = map.getOverlays();
+      var i = 0;
+      while(i < markers.length) {
+        map.removeOverlay(markers[i]);
+        i++;
+      }
+
       map.addOverlay(marker);
-      map.centerAndZoom(point, 16);
+      map.centerAndZoom(point, 18);
     }
   });
 
-  geoFindMe();
+  findFirends();
   setInterval(geoFindMe, 5000);
+
+  function findFriends() {
+    var geoQuery = geoFire.query({
+      center: []
+    });
+  }
 
   function updateLocation(position) {
     dataRef.set({
@@ -35,13 +58,29 @@
     };
 
     var error = function() {
-      alert("Unable to retrieve your location");
+      console.log("Unable to retrieve your location");
     };
 
     navigator.geolocation.getCurrentPosition(success, error);
   }
 
   function translateCallback(position) {
+    if($("#messageBox").children().length > 10) {
+      $("#messageBox").empty();
+    }
+
+    var currentTime = new Date();
+    $("<p>" + currentTime.toLocaleTimeString() + " 经度: " + position.lng + ", 纬度: " + position.lat + "</p>").prependTo("#messageBox");
     updateLocation({longitude: position.lng, latitude: position.lat});
+    geoFire.set(person, [position.lat, position.lng]);
+
+    if(geoQuery === undefined){
+      geoQuery = geoFire.query({
+        center: [position.lat, position.lng],
+        radius: 5000
+      });
+
+      geoQuery.on();
+    }
   }
 })(window.jQuery);
